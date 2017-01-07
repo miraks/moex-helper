a8633c902a0207dd802ed4c0af38969b9bc04b2a
miraks <a@vldkn.net>
2017-01-07 14:15:43 +0300

    Run quantum tasks only in production

--- config/config.exs
+++ config/config.exs
@@ -22,19 +22,4 @@ config :guardian, Guardian,
   secret_key: File.read!("config/#{Mix.env}.secret.key"),
   serializer: MoexHelper.GuardianSerializer
 
-config :quantum, cron: [
-  security_sync: [
-    schedule: "@hourly",
-    task: {MoexHelper.Tasks.SyncSecurities, :call}
-  ],
-  email_stats: [
-    schedule: "10 7 * * *",
-    task: {MoexHelper.Tasks.EmailStats, :call}
-  ],
-  create_coupons: [
-    schedule: "10 6 * * *",
-    task: {MoexHelper.Tasks.CreareCoupons, :call}
-  ]
-]
-
 import_config "#{Mix.env}.exs"
--- config/prod.exs
+++ config/prod.exs
@@ -6,4 +6,19 @@ config :moex_helper, MoexHelper.Endpoint,
   server: true,
   version: Mix.Project.config[:version]
 
+config :quantum, cron: [
+  security_sync: [
+    schedule: "@hourly",
+    task: {MoexHelper.Tasks.SyncSecurities, :call}
+  ],
+  email_stats: [
+    schedule: "10 7 * * *",
+    task: {MoexHelper.Tasks.EmailStats, :call}
+  ],
+  create_coupons: [
+    schedule: "10 6 * * *",
+    task: {MoexHelper.Tasks.CreareCoupons, :call}
+  ]
+]
+
 import_config "prod.secret.exs"
